18c3bce654cafc71562758227a85726ea12f0d64
teekaytech <taofeek.olalere@feghas.com>
2020-09-25 21:36:27 +0100

    Add array-analysis module and test file

--- src/__tests__/caesarCipher.test.js
+++ src/__tests__/caesarCipher.test.js
@@ -3,14 +3,20 @@ import caesarCipher from '../modules/caesarCipher';
 let text;
 let encoded;
 
-test('decipher() upper case string', () => {
+test('caesar() upper case string', () => {
   text = 'WELCOME TO JEST TESTING';
-  encoded = caesarCipher.decipher(text, 1);
+  encoded = caesarCipher.caesar(text, 1);
   expect(encoded).toEqual('XFMDPNF UP KFTU UFTUJOH');
 });
 
-test('decipher() lower case string', () => {
+test('caesar() lower case string', () => {
   const text = "let's see if this works";
-  const encoded = caesarCipher.decipher(text, 2);
+  const encoded = caesarCipher.caesar(text, 2);
   expect(encoded).toEqual("ngv'u ugg kh vjku yqtmu");
+});
+
+test('caesar() with punctuations', () => {
+  const text = 'Are you okay? Asked the teacher.';
+  const encoded = caesarCipher.caesar(text, 3);
+  expect(encoded).toEqual('Duh brx rndb? Dvnhg wkh whdfkhu.');
 });
--- src/modules/caesarCipher.js
+++ src/modules/caesarCipher.js
@@ -2,15 +2,15 @@ const CaesarCipher = (() => {
   const getCipheredChar = (origin, key, code) => String.fromCharCode(origin
     + ((code - origin + key) % 26));
 
-  const decipher = (text, key) => {
+  const caesar = (text, key) => {
     let output = '';
     for (let i = 0; i < text.length; i += 1) {
       const currentCharCode = text[i].charCodeAt();
       // handling lowercase character
-      if ((currentCharCode <= 90) && (currentCharCode >= 65)) {
+      if (currentCharCode <= 90 && currentCharCode >= 65) {
         output += getCipheredChar(65, key, currentCharCode);
         // handling uppercase character
-      } else if ((currentCharCode <= 122) && (currentCharCode >= 97)) {
+      } else if (currentCharCode <= 122 && currentCharCode >= 97) {
         output += getCipheredChar(97, key, currentCharCode);
       } else {
         // handling other characters
@@ -20,7 +20,7 @@ const CaesarCipher = (() => {
     return output;
   };
 
-  return { decipher };
+  return { caesar };
 })();
 
 export default CaesarCipher;
